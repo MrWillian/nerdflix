f6fa465482fc659f63fd1cfef9d774667dc79107
MrWilllian <willian.dev10@gmail.com>
2020-07-29 16:39:55 -0300

    refactor: Cadastro Categoria form

--- src/pages/Cadastro/Categoria/index.tsx
+++ src/pages/Cadastro/Categoria/index.tsx
@@ -1,17 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+
+import { Container, Form, SubmitButton } from './styles';
+
+interface PropsValues {
+  nome: string;
+  descricao: string;
+  cor: string;
+}
 
 const CadastroCategoria: React.FC = () => {
+  const valoresIniciais = { nome: '', descricao: '', cor: '' }
+  const [categorias, setCategorias] = useState<PropsValues[]>([]);
+  const [values, setValues] = useState<PropsValues>(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({ ...values, [chave]: valor });
+  }
+
+  function handleChange(event) {
+    setValue(
+      event.target.getAttribute('name'),
+      event.target.value
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCategorias([...categorias, values]);
+    setValues(valoresIniciais);
+  }
+
   return (
     <PageDefault>
 
-      <h1>Cadastro de Categoria</h1>
+      <Container>
+        <h2>Cadastro de Categoria: {values.nome}</h2>
 
-      <Link to="/">
-        Ir para home
-      </Link>
+        <Form onSubmit={handleSubmit}>
+          <FormField
+            label="Nome da Categoria"
+            type="text"
+            name="nome"
+            value={values.nome}
+            onChange={handleChange}
+          />
+
+          <FormField
+            label="Descrição"
+            type="textarea"
+            name="descricao"
+            value={values.descricao}
+            onChange={handleChange}
+          />
+
+          <FormField
+            label="Cor"
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange={handleChange}
+          />
+            
+          <SubmitButton>Cadastrar</SubmitButton>
+        </Form>
+
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+              <li key={`${categoria}${indice}`}>
+                {categoria.nome}
+              </li>
+            );
+          })}
+        </ul>
+
+        <Link to="/">
+          Ir para home
+        </Link>
+
+      </Container>
     </PageDefault>
   );
 }
