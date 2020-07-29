02f9f8af183cc376bc58823401fe0a38ab8fe411
MrWilllian <willian.dev10@gmail.com>
2020-07-29 10:52:34 -0300

    refactor: add Link component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Container } from './styles';
 
 const Button: React.FC = () => {
   return (
-    <Container as="a" className="ButtonLink" href="/">
+    <Container as={Link} className="ButtonLink" to="/cadastro/video">
       Novo vídeo
     </Container>
   );
