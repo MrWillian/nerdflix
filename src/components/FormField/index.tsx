da363d10f0f7b34d32595152d4b1ebd82fbdd7ec
MrWilllian <willian.dev10@gmail.com>
2020-07-30 15:56:59 -0300

    refactor: change Form Field

--- src/components/FormField/index.tsx
+++ src/components/FormField/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { CustomInput, CustomTextarea } from './styles';
+import { Container, CustomInput, CustomTextarea, Text } from './styles';
 
 interface Props {
   label?: string;
   type?: string;
   name?: string;
-  value?: string;
+  value: string;
   onChange(event: any): void;
 }
 
@@ -13,24 +13,26 @@ const FormField: React.FC<Props> = ({
   label, type, name, value, onChange 
 }) => {
   return (
-    <div>
+    <Container>
       <label>
         {type != 'textarea' ? 
           <CustomInput
             type={type}
             value={value}
             name={name}
-            placeholder={label}
             onChange={onChange} />
           : 
           <CustomTextarea
             value={value}
             name={name}
-            placeholder={label}
             onChange={onChange} />
         }
+        <Text>
+          {label}
+          :
+        </Text>
       </label>
-    </div>
+    </Container>
   )
 }
 
